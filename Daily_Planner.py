0f445c472340955d7879194abd12c8ce1a621fe3
unplugged001 <ochuwilliamsirawo@gmail.com>
2023-05-19 19:46:21 +0100

    Dailly_Planner-v1 added

---
+++ Daily_Planner.py
@@ -0,0 +1,271 @@
+## Daily Planner 
+## This script will be automated and run daily by 7:00GMT
+## This script will help the administrator plan his day and tasks
+## It will serve as a daily journal to help the staff log his activities
+## The staff will enter in his tasks
+## Any task fulfiled should be cleared manually.
+## Any task not cleared will be carried over to the next day
+## Enjoy
+
+## SO therefore, lets cook 
+
+import random
+import os
+import time
+import datetime
+import sys
+
+tasks=[]
+todays_date=datetime.date.today()
+
+## lets get the staff details
+## These details will be used all through the program 
+
+staff_name=input("Enter your name: ")
+staff_dept=input("Enter the department you work in: ")
+staff_time=input('What is the time as at now! : ')
+
+print('\n')
+print("\033[1mHello {}\033[0m, of \033[1m{}\033[0m department. Lets help you plan your day".format(staff_name.title(), staff_dept.title()))
+print('\n')
+print("Today you begin at \033[1m{}\033[0m:hrs ".format(staff_time))
+print("Ensure you work hard, have fun and make history!!!")
+print('\n')
+    
+def add_task_staff():
+    print('\n')
+    print("\033[1mHello Proceed to add your tasks: \033[0m")
+    print('\n')
+    print("Type exit to return to start menu")
+    print('\n')
+    
+    ## Lets Make this repeat till the user cancels it 
+    while True:  
+        
+        ## Initiate New task
+        new_task=input("Add a new task: ")
+        
+        if new_task.lower()=="exit":
+            print('\n')
+            print("We will return to main menu in 5 seconds")
+            countdown_indicator()
+            print('\n')
+            pick_options()
+            
+        else:
+            tasks.append(new_task)
+            print("\033[1m{}\033[0m has been added".format(new_task))
+            time.sleep(0.25)
+        
+        
+    
+def remove_task_staff():
+    print('\n')
+    print('Select the task you want to remove')
+    print(' '*15, "or")
+    print("Type exit to return to start menu")
+    print('\n')
+    
+    
+    while True:
+        count=0
+        ## Lets us print all the available tasks
+        for task in tasks:
+            count+=1
+            print("{}.{}".format(count,task))
+            
+        ## Let us setup the conditionals to remove task
+        print('\n')
+        to_be_removed=input("Enter the task number you want removed: ")
+        
+        
+        if to_be_removed.lower()=="exit":
+            print('We will return you to main menu in 5 seconds!!')
+            countdown_indicator()
+            pick_options()
+        
+        ## lets now remove the items
+        to_be_removed_index=int(to_be_removed)
+            
+        ## Let us remove some tasks
+        print('\n')
+        
+        ##item_position=tasks.index(to_be_removed)
+        ## Let us try to implement a try-except block to prevent this from throwing an error
+        
+        try:    
+            removed_task=tasks.pop(to_be_removed_index-1)
+        except IndexError:
+            time.sleep(0.5)
+            print("ERROR!!.....No such task in the list")
+            print('\n')
+            
+        else:        
+            print("\033[1m{}\033[0m has been removed".format(removed_task))
+            time.sleep(0.25)
+            print('\n')
+        
+    
+    
+def edit_task_staff():
+    ## We will try to make edits to any additions that has been done
+    print('\n')
+    print('Which task do you want to make changes to: ')
+    print(' '*20, "or")
+    print("Type exit to return to start menu")
+    print('\n')
+    
+    ## We will keep the program running till the user specifies otherwise
+    
+    while True:
+        
+        count=0
+        
+    
+        for task in tasks:
+            count+=1
+            print("{}.{}".format(count,task)) 
+            
+        ## Let us initiate the change 
+        print('\n')
+        to_be_replaced=input("select a task to be modified: ")
+        
+        if to_be_replaced.lower()=="exit":
+            print("We will return you to the main menu in 5 seconds")
+            countdown_indicator()
+            pick_options()
+        
+    
+        to_be_replaced_index=int(to_be_replaced)
+        
+        ##item_position=tasks.index(to_be_replaced)
+        
+        print('\n')
+        
+        ## Now we get the index and change the task 
+        replacement_value=input("Enter your new task ")
+        
+        try:
+            old_value=tasks[to_be_replaced_index-1]
+            tasks[to_be_replaced_index-1]=replacement_value
+        except:
+            time.sleep(0.5)
+            print('ERROR..!!   No such task exists')
+            print('\n')
+        else:
+            print('\n')
+            print("\033[1m{}\033[0m has been replaced with \033[1m{}\033[0m".format(old_value, replacement_value))
+            time.sleep(0.25)
+            print('\n')
+        
+    
+
+def view_task_staff():
+    ## We will now display all the tasks the staff inputed
+    
+    print('\n')
+    print("Here is a list of all your tasks sceduled for \033[1m{}\033[0m".format(todays_date))
+    print('\n')
+    
+    
+    for task in tasks:
+        task_num=tasks.index(task)
+        print("{}. {}".format(task_num+1, task))
+    
+    print("\n")
+    
+    exit_view=input("You can type exit to return to the main menu: ")
+    if exit_view.lower()=="exit":
+        print("We will return you to the main menu in 5 seconds")
+        countdown_indicator()
+        pick_options()
+    
+    
+        
+def exit_task_staff():
+    ## We will use this function to exit the program
+    print('\n')
+    print("Thanks for your time, Apllication will close in 5 seconds ")
+    countdown_indicator()
+    print("\n")
+    sys.exit()
+    
+
+
+def countdown_indicator():
+    n=0
+    while n<=4:
+        print("\033[1m.\033[0m", end=" ")
+        time.sleep(0.5)
+        n+=1 
+    
+    
+   
+    
+## Here we have initiated our four functionalities
+## We shall pause for tonigt and then conpound eeverything tomorrow morning
+## Cheers amigos
+
+## Welcome to a new day, Now we strive to complete the cooking
+
+def welcome_greeting():
+    welcome_greetings="DAILY_PLANNER  📝📝"
+    print("\033[1m \033[40m{: ^200}\033[0m" .format(welcome_greetings))
+    
+    
+def pick_options():
+    print('\n')
+    print("Pls select an option by entering the event number: ")
+    print("\n")
+    
+    ## Lets put all the available options in a dictionary
+    
+    options={1:"Add New Task", 2:"Remove a Task", 3:"Edit a Task", 4:"View Tasks", 5:"Exit Daily Planner"}
+    
+    ## Lets make a list of all the keys
+    list_of_options_keys=list(options.keys())
+    
+    ## Now let us print out all the optons
+    ## We might get fancy 
+    
+    for id,option in options.items():
+        print("{}. {}".format(id, option))
+        
+    print('\n')
+    
+    
+    ## lets print out the list of possible options n
+    
+    options_pick=input("Select the event number: ")
+    options_pick1=int(options_pick)
+    
+    
+    ## At this stage we define what happens when each option is selected
+    
+    
+    if options_pick1==list_of_options_keys[0]:    
+        ## Call the add_task function
+        add_task_staff()
+        
+    elif options_pick1==list_of_options_keys[1]:
+        ## call the remove task function
+        remove_task_staff()
+        
+    elif options_pick1==list_of_options_keys[2]:
+        ## Call the edit task function
+        edit_task_staff()
+        
+    elif options_pick1==list_of_options_keys[3]:
+        ## Call the view tsk function
+        view_task_staff()
+        
+    elif options_pick1==list_of_options_keys[4]:
+        ## call the exit daily planner function
+        exit_task_staff()
+        
+        
+welcome_greeting()
+pick_options()
+        
+        
+    
---
+++ README.md
@@ -0,0 +1,15 @@
+# Daily_Planner_v1!
+
+This is a  **Daily_Planner_v1** that will allow users plan their day by sceduling tasks to be done</br> Users can add tasks, remove tasks and also edit tasks seamlessly with this application</br>The application is interactive and can be easily used by all walks of life 
+This project is to demonstrate my flexibility with the basics of python which include variables, functions and conditionals. <br/> 
+Feel free to play with it and hit me back. 
+I will be modifying this as I continue to progress in my learning. 
+Cheers evryone  
+
+
+
+## How to use 
+
+ - Run the application and input your personal details
+ - Proceed to interact with the user menu and select the functionality you would like to perform
+ - Enjoy your useage  
